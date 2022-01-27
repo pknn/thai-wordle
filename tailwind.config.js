a6a6681649aad8272de2dd10255b09f0d84a025f
Pakanon Pantisawat <pakanon.pantisawat@gmail.com>
2022-01-27 21:04:04 +0700

    feat: added animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,19 @@ module.exports = {
     container: {
       center: true,
     },
+    animation: {
+      'cell-in': 'cell-in .2s linear',
+    },
+    keyframes: {
+      'cell-in': {
+        '0%, 100%': {
+          transform: 'scale(1)',
+        },
+        '50%': {
+          transform: 'scale(1.1)',
+        },
+      },
+    },
     extend: {},
   },
   plugins: [],
